51bf0b1d652636c2df12e307c227c80bcf009c05
sree <sreelekhapellakuru@gmail.com>
2019-11-06 15:25:28 +0530

    add arraylist3

---
+++ Collections/src/com/collections/Arraylist3.java
@@ -0,0 +1,22 @@
+package com.collections;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Arraylist3 {
+	public static void main(String[] args)
+	{
+		LinkedList al = new LinkedList();
+		al.add(10);
+		al.add(200);
+		al.add(31);
+		al.add(300);
+		al.add(100);
+		
+		Iterator itr = al.descendingIterator();
+		while(itr.hasNext())
+		{
+			System.out.println(itr.next());
+		}
+	}
+}
